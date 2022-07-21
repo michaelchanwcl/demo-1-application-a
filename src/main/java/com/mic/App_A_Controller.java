6cd68fef89c8c61332c01e7e0f482540b1faf6c2
Michael <michael.chan@wclsolution.com>
2022-07-21 09:52:34 +0800

    Commit 1

--- src/main/java/com/mic/App_A_Controller.java
+++ src/main/java/com/mic/App_A_Controller.java
@@ -1,5 +1,7 @@
 package com.mic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App_A_Controller {
 
+    private static final Logger log = LoggerFactory.getLogger(App_A_Controller.class);
+
     @Autowired
     App_B_Proxy appBProxy;
 
@@ -15,6 +19,7 @@ public class App_A_Controller {
     public App_A_ResponseDTO post(@RequestBody App_A_RequestDTO appARequestDTO) {
         App_B_RequestDTO appBRequestDTO = new App_B_RequestDTO();
         appBRequestDTO.setA("Value from A Request=[" + appARequestDTO.getA() + "]" );
+        log.info("Calling App B Proxy with request parameter=[" + appBRequestDTO.getA() + "]");
         App_B_ResponseDTO appBResponseDTO = appBProxy.retrieveValue(appBRequestDTO);
 
         App_A_ResponseDTO appAResponseDTO = new App_A_ResponseDTO();
